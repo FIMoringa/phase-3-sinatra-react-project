3a647fd801129cfd8231fb02b05c85f79aa56aa2
Titus Muthomi <muthomititus+github@gmail.com>
2022-12-03 15:16:59 +0300

    Mock backend

---
+++ frontend/src/posts.js
@@ -0,0 +1,51 @@
+export const posts = [
+	{
+		id: 1,
+		title: 'Post Title one',
+		content:
+			' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, quas.',
+		author: 'author one',
+	},
+	{
+		id: 2,
+		title: 'Post Title two',
+		content:
+			' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, quas.',
+		author: 'author two',
+	},
+	{
+		id: 3,
+		title: 'Post Title three',
+		content:
+			' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, quas.',
+		author: 'author three',
+	},
+	{
+		id: 4,
+		title: 'Post Title four',
+		content:
+			' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, quas.',
+		author: 'author four',
+	},
+	{
+		id: 5,
+		title: 'Post Title five',
+		content:
+			' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, quas.',
+		author: 'author five',
+	},
+  {
+		id: 6,
+		title: 'Post Title six',
+		content:
+			' Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, quas.',
+		author: 'author five',
+	},
+];
+
+export const authors = [
+	{ id: 1, first_name: 'author one', last_name: 'Lorem ipsum d' },
+	{ id: 2, first_name: 'author two', last_name: 'Lorem ipsum d' },
+	{ id: 3, first_name: 'author three', last_name: 'Lorem ipsum d' },
+	{ id: 4, first_name: 'author four', last_name: 'Lorem ipsum d' },
+];
